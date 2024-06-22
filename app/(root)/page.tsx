c4169ee58acc851e7d19982df8abffc47ed98c0e
Yogesh Bisht <yogesh.bisht@iotfy.co>
2024-06-23 03:00:57 +0530

    revalidate value changed to 3600

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -9,7 +9,7 @@ import HeroImage from "@/assets/images/hero.png";
 import FeatureArticleCard from "./FeatureArticles";
 
 
-export const revalidate = 300;
+export const revalidate = 3600;
 
 
 export const metadata: Metadata = {
