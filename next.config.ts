589fe4a4a2d99504bbfa6c30d8a64dded7cebcc3
Yogesh Bisht <yogeshbisht.2307@gmail.com>
2026-03-27 18:43:58 +0530

    next config changes related to deployment

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "standalone",
+  outputFileTracingIncludes: {
+    '/**': ['./generated/prisma/**/*'],
+  },
   images: {
     remotePatterns: [
       {
