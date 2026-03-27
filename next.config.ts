c277dd978d778d90eb86a9c83aa9f19dd7712b13
Yogesh Bisht <yogeshbisht.2307@gmail.com>
2026-03-27 19:27:41 +0530

    add command to copy generate to .next folder after build

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "standalone",
-  outputFileTracingIncludes: {
-    '/**': ['./generated/prisma/**/*'],
-  },
   images: {
     remotePatterns: [
       {
